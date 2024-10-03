8e23dade2818de801ae4cc0f01cf7c3bbbd25a07
David Riva <davidjriva@gmail.com>
2024-10-02 21:59:12 -0700

    use material icons

--- next-email-app/src/components/TextInputField.js
+++ next-email-app/src/components/TextInputField.js
@@ -1,23 +1,35 @@
-import { TextField, Button, Box } from "@mui/material";
-import React from "react";
+import { TextField, IconButton, Box } from "@mui/material";
+import React, { useState } from "react";
+import FormatListBulletedIcon from "@mui/icons-material/FormatListBulleted";
+import FormatListNumberedIcon from "@mui/icons-material/FormatListNumbered";
 
 const TextInputField = ({ text, setText }) => {
+  const [numberedCount, setNumberedCount] = useState(0); // State to track number of numbered points
+
   const handleChange = (e) => {
     setText(e.target.value);
   };
 
   const handleAddBulletPoint = () => {
-    setText((prevText) => `${prevText}\n• `); // Add bullet point
+    setText((prevText) => `${prevText}\n• `);
   };
 
   const handleAddNumberedPoint = () => {
-    const pointCount =
-      text.split("\n").filter((line) => line.trim() !== "").length + 1;
-    setText((prevText) => `${prevText}\n${pointCount}. `); // Add numbered point
+    const newCount = numberedCount + 1;
+    setNumberedCount(newCount);
+    setText((prevText) => `${prevText}\n${newCount}. `);
   };
 
   return (
     <>
+      <Box sx={{ marginTop: 2 }}>
+        <IconButton onClick={handleAddBulletPoint} sx={{ marginRight: 1 }}>
+          <FormatListBulletedIcon />
+        </IconButton>
+        <IconButton onClick={handleAddNumberedPoint}>
+          <FormatListNumberedIcon />
+        </IconButton>
+      </Box>
       <TextField
         label="Enter your text"
         variant="outlined"
@@ -28,18 +40,6 @@ const TextInputField = ({ text, setText }) => {
         value={text}
         onChange={handleChange}
       />
-      <Box sx={{ marginTop: 2 }}>
-        <Button
-          variant="outlined"
-          onClick={handleAddBulletPoint}
-          sx={{ marginRight: 1 }}
-        >
-          Add Bullet Point
-        </Button>
-        <Button variant="outlined" onClick={handleAddNumberedPoint}>
-          Add Numbered Point
-        </Button>
-      </Box>
     </>
   );
 };

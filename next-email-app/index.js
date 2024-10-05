7bab431642afbd9b581cbcd937013d4a234b55ad
David Riva <davidjriva@gmail.com>
2024-10-04 18:56:41 -0700

    Format example email

--- next-email-app/index.js
+++ next-email-app/index.js
@@ -4,44 +4,24 @@ const { exec } = require("child_process");
 
 // Function to execute AppleScript for sending email via Outlook
 function sendEmailWithOutlook(name, email, text) {
-  const htmlTemplate = `
-        <html>
-            <body>
-                <h1>Hello ${name},</h1>
-                <p>Thank you for providing your email address: ${email}.</p>
-                <p>Here is some text:</p>
-                <p>${text.replace(/\*/g, "•")}</p>
-                <p>Best regards,<br>Your Company</p>
-            </body>
-        </html>
-    `.trim(); // Trim to remove extra whitespace
-
   const appleScript = `
-    set {ccName01, ccAddress01} to {"LocalDispatch", "change-Alias@change-email.com"} -- 'Cc:' recipient.
-    set {ccName02, ccAddress02} to {"ChangeName", "change-Alias@change-email.com"} -- 'Cc:' recipient.
-    set {ccName03, ccAddress03} to {"ChangeName", "change-Alias@change-email.com"} -- 'Cc:' recipient.
-    set the_Subject to "Dispatch Complaint Email"
-    set the_Content to ("<b>" & "Date:" & "</b>" & "<br><br>" & "<b>" & "Complaint Department:" & "</b>" & "<br><br><br>" & "<b>" & "Call received on 311 or 911:" & "</b>" & "<br><br><br>" & "<b>" & "Person Identifying Complaint:" & "</b>" & "<br><br><br>" & "<b>" & "Complaint:" & "</b>")
+    set {ccName01, ccAddress01} to {"Example CC", "exampleCC@example.com"} -- 'Cc:' recipient.
+    
+    set the_Subject to "Example Subject"
+
+    set the_Content to ("<div>" & "Hello ${name}" & "</div>" & "<br>" & "Thank you for opting in for an email to ${email}" & "</br>" & "<br>" & "</br>" & "<br>" & "Here is some text:" & "</br>" & "<br>" & "${text}" & "</br>" & "<br>" & "</br>" & "<br>" & "</br>" & "<div>" & "Best," & "</div>")
 
     tell application "Microsoft Outlook"
         
     set ComplaintMessage to make new outgoing message with properties {subject:the_Subject, content:the_Content}
     
     make new cc recipient at ComplaintMessage with properties {email address:{name:ccName01, address:ccAddress01}}
-    make new cc recipient at ComplaintMessage with properties {email address:{name:ccName02, address:ccAddress02}}
-    make new cc recipient at ComplaintMessage with properties {email address:{name:ccName03, address:ccAddress03}}
 
     open ComplaintMessage
 
     end tell
     `;
 
-  // HTML CODE:
-  // set content of newMessage to "<html><body>${htmlTemplate
-  //     .replace(/"/g, '\\"')
-  //     .replace(/\n/g, "")}</body></html>"
-
-  // Execute AppleScript
   exec(`osascript -e '${appleScript}'`, (error, stdout, stderr) => {
     if (error) {
       console.error(`Error executing AppleScript: ${error.message}`);
